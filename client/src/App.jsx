cefc8ed92d28b85bdbd2277b5c2112dfee8fcd45
Dilek Nuredin <dileknuredin@hotmail.com>
2024-04-29 19:33:56 -0400

    added basic for for CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Taskr from './pages/home'
+
+function App() {
+    return (
+      <div className="taskr-app">
+        < Taskr />
+      </div>
+    );
+  }
+  
+  export default App;
+  
