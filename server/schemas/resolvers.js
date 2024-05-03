b4d6f72f760efecc34991bba23581e02836408ce
caiman48 <caiman48clm@gmail.com>
2024-05-02 20:35:59 -0400

    created typeDefs.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
-const User = require('../models/User');
-const Task = require('../models/Task');
+const { User, Task } = require('../models');
+
 const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
