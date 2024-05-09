b2a8e772ba3f3d54a81334c1bec8140663e256be
Luke S. <lukestroehlein18@gmail.com>
2024-05-09 18:32:04 -0400

    fixed addUser mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -41,7 +41,7 @@ const typeDefs = `
 
   type Mutation {
     login(email: String!, password: String!): Auth
-    addUser(firstName: String!, lastName: String!, username: String!, email: String!, password: String!): Auth
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     addTask(taskdata: TaskInput!): User
     updateTask(taskId: ID!, title: String, description: String, dueDate: String, completed: Boolean): Task
     deleteTask(taskId: ID!): User
