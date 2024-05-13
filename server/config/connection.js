bb0ac519c9073a80152153022abb34bd4d69c5b7
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-05-13 19:47:27 -0400

    pushing code to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.PORT || 'mongodb://127.0.0.1:27017/taskr');
+mongoose.connect(process.env.PORT || "mongodb://127.0.0.1:27017/taskr");
+console.log("Connecting to MongoDB with URI:", process.env.MONGO_URI);
 
 module.exports = mongoose.connection;
