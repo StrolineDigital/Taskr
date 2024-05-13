0b20e0351d570c4ed33f32fb67bb4593cb0ec672
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-05-13 15:50:45 -0400

    refractered signup

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -1,29 +1,32 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 //This function will allow users to sign up for the application
 function Signup() {
-  const [formState, setFormState] = useState({ email: '', password: '' });
-  const [addUser] = useMutation(ADD_USER);
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [createUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const mutationResponse = await addUser({
+    console.log(formState);
+    const { data } = await createUser({
       variables: {
-        email: formState.email,
-        password: formState.password,
-        firstName: formState.firstName,
-        lastName: formState.lastName,
+        ...formState,
       },
     });
+
+    if (!data) {
+      throw new Error("No data received from server. Please try again later.");
+    }
     //use the token from the mutation response to log the user in
-    console.log(mutationResponse);
-    const token = mutationResponse.data.addUser.token;
+    console.log(data);
+    const token = data.addUser.token;
+    console.log(token);
     Auth.login(token);
   };
-//This function will allow users to enter their first name, last name, email, and password to sign up
+  //This function will allow users to enter their first name, last name, email, and password to sign up
   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormState({
@@ -31,7 +34,7 @@ function Signup() {
       [name]: value,
     });
   };
-//This will render the signup form
+  //This will render the signup form
   return (
     <div className="container my-1">
       <Link to="/login">← Go to Login</Link>
