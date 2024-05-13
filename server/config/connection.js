bacc632129db21c1295f3e736128c66002f1bb53
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-05-13 19:54:03 -0400

    Fix MongoDB connection string reference

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.PORT || "mongodb://127.0.0.1:27017/taskr");
-console.log("Connecting to MongoDB with URI:", process.env.MONGO_URI);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/taskr");
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log('MongoDB URI:', process.env.MONGODB_URI);
 
 module.exports = mongoose.connection;
