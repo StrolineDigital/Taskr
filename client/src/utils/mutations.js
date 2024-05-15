542871e32e2d89d1e23fd8a4b7f085108e7226f0
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-05-15 15:56:47 -0400

    updated mutations and quaries on the front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,75 +1,77 @@
 import { gql } from "@apollo/client";
 //This file holds the mutations that will be used in the application
 export const LOGIN = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-      }
-    }
-  }
+ mutation login($email: String!, $password: String!) {
+   login(email: $email, password: $password) {
+     token
+     user {
+       _id
+     }
+   }
+ }
 `;
 
+
 export const ADD_USER = gql`
-  mutation addUser(
-    $firstName: String!
-    $lastName: String!
-    $email: String!
-    $password: String!
-  ) {
-    addUser(
-      firstName: $firstName
-      lastName: $lastName
-      email: $email
-      password: $password
-    ) {
-      token
-      user {
-        _id
-        
-      }
-    }
-  }
+ mutation addUser(
+   $firstName: String!
+   $lastName: String!
+   $email: String!
+   $password: String!
+ ) {
+   addUser(
+     firstName: $firstName
+     lastName: $lastName
+     email: $email
+     password: $password
+   ) {
+     token
+     user {
+       _id
+      
+     }
+   }
+ }
 `;
 export const ADD_TASK = gql`
-mutation Mutation($taskdata: TaskInput!) {
-  addTask(taskdata: $taskdata) {
-    _id
-    firstName
-    lastName
-    email
-    tasks {
-      _id
-    }
-  }
+mutation addTask($taskdata: TaskInput!) {
+ addTask(taskdata: $taskdata) {
+   _id
+   tasks {
+     _id
+     text
+     isComplete
+   }
+ }
 }
 `;
+
+
 export const UPDATE_TASK = gql`
 mutation updateTask($taskId: ID!, $taskdata: TaskInput!) {
-  updateTask(taskId: $taskId, taskdata: $taskdata) {
-    _id
-    firstName
-    lastName
-    email
-    tasks {
-      _id
-    }
-  }
+ updateTask(taskId: $taskId, taskdata: $taskdata) {
+   _id
+   firstName
+   lastName
+   email
+   tasks {
+     _id
+     text
+     isComplete
+   }
+ }
 }
-`; 
+`;
+
 
 export const DELETE_TASK = gql`
 mutation deleteTask($taskId: ID!) {
-  deleteTask(taskId: $taskId) {
-    username
-    tasks {
-      _id
-      title
-      description
-      dueDate
-      completed
-    }
-  }
+ deleteTask(taskId: $taskId) {
+   email
+   tasks {
+     _id
+     text
+   }
+ }
 }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,30 +1,27 @@
 import { gql } from '@apollo/client';
 //This file holds the queries that will be used in the application
 export const QUERY_USER = gql`
-  {
-    me {
-      firstName
-      lastName
-      email
-      tasks {
-        _id
-        title
-        description
-        dueDate
-        completed
-      }
-    }
-  }
-`; 
+ {
+   me {
+     firstName
+     lastName
+     email
+     tasks {
+       _id
+       text
+       isComplete
+     }
+   }
+ }
+`;
+
 
 export const QUERY_TASKS = gql`
-  {
-    gettask {
-      _id
-      title
-      description
-      dueDate
-      completed
-    }
-  }
+ {
+   gettask {
+     _id
+     text
+     isComplete
+   }
+ }
 `;
